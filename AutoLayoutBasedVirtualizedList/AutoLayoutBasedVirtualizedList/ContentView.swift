7a7fb3107d85db4b7ea49dc38ad704361004b191
Liu, Minsheng <lambda@liu.ms>
2020-05-23 19:59:02 +0800

    add an Auto Layout based virtualized list.

--- AutoLayoutBasedVirtualizedList/AutoLayoutBasedVirtualizedList/ContentView.swift
+++ AutoLayoutBasedVirtualizedList/AutoLayoutBasedVirtualizedList/ContentView.swift
@@ -1,21 +1,263 @@
-//
-//  ContentView.swift
-//  AutoLayoutBasedVirtualizedList
-//
-//  Created by 刘闽晟 on 2020/5/22.
-//  Copyright © 2020 刘闽晟. All rights reserved.
-//
+import UIKit
 
-import SwiftUI
+let acceptableSizes: [CGFloat] = [50, 100, 150, 200]
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+class Size {
+    var value: CGFloat = acceptableSizes[0]
+    func step() {
+        value = acceptableSizes.randomElement()!
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class Cell: UIView {
+    var id: Int! {
+        didSet {
+            if let id = id {
+                label.text = "\(id)"
+            }
+        }
+    }
+    var size: Size!
+    var heightConstraint: NSLayoutConstraint!
+
+    let label = UILabel()
+    var labelConstraints: [NSLayoutConstraint]?
+
+    override func updateConstraints() {
+        if labelConstraints == nil {
+            label.translatesAutoresizingMaskIntoConstraints = false
+            var constraints = [NSLayoutConstraint]()
+            constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|", metrics: nil, views: ["view": label])
+            constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", metrics: nil, views: ["view": label])
+            NSLayoutConstraint.activate(constraints)
+            labelConstraints = constraints
+        }
+
+        if let constraint = heightConstraint {
+            constraint.constant = self.size.value
+        } else {
+            heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: size.value)
+            addConstraint(heightConstraint)
+        }
+
+        super.updateConstraints()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(label)
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title1)
+        backgroundColor = .systemPink
+    }
+
+    required init?(coder: NSCoder) {
+        return nil
+    }
+
+    private var timer: Timer?
+
+    func attach(id: Int, size: Size) {
+        self.id = id
+        self.size = size
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print("Cell layout done for cell \(id!).")
+    }
+}
+
+class RootViewController: UIViewController {
+    let scrollView = UIScrollView()
+    let listView = HackedView()
+    let topView = UIView()
+    let bottomView = UIView()
+    var topHeightConstraint: NSLayoutConstraint!
+    var bottomHeightConstraint: NSLayoutConstraint!
+
+    func configureHierarchy() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+
+        for view in [listView, topView, bottomView] {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            scrollView.addSubview(view)
+        }
+
+        let views = ["scroll": scrollView, "list": listView, "top": topView, "bottom": bottomView]
+        var constraints = [NSLayoutConstraint]()
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-[scroll]-|", metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|[scroll]|", metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[list]|", metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[top]|", metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|[bottom]|", metrics: nil, views: views)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-[top][list][bottom]-|", metrics: nil, views: views)
+        constraints.append(NSLayoutConstraint(item: scrollView, attribute: .width, relatedBy: .equal, toItem: listView, attribute: .width, multiplier: 1, constant: 0))
+        NSLayoutConstraint.activate(constraints)
+
+        scrollView.contentInsetAdjustmentBehavior = .never
+
+        listView.axis = .vertical
+        listView.alignment = .fill
+        listView.distribution = .fill
+        listView.spacing = 8
+
+        topHeightConstraint = NSLayoutConstraint(item: topView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: 0)
+        topView.addConstraint(topHeightConstraint)
+        bottomHeightConstraint = NSLayoutConstraint(item: bottomView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: 0)
+        bottomView.addConstraint(bottomHeightConstraint)
+    }
+
+    var sizes = (0..<100).map { _ in Size() }
+    // Intentionally wrong initial estimates.
+    var cachedSizes = Array(repeating: CGFloat(25), count: 100)
+    var visibleCells = [Int: Cell]()
+    var pool = Set<Cell>()
+
+    func nextCell() -> Cell {
+        if let first = pool.popFirst() {
+            return first
+        }
+        return Cell()
+    }
+
+    var visualOffset: (Int, CGFloat)?
+
+    func updateVisibleCells() {
+        let top = scrollView.contentOffset.y
+        let bottom = top + scrollView.bounds.height
+
+        struct Changes {
+            var invalidatedCachedSizes = [Int]()
+            var insertedIndices = [Int]()
+            var removedIndices = [Int]()
+
+            var isEmtpy: Bool {
+                invalidatedCachedSizes.count == 0 && insertedIndices.count == 0 && removedIndices.count == 0
+            }
+        }
+        var changes = Changes()
+
+        for (index, cell) in visibleCells {
+            if cachedSizes[index] != cell.bounds.height {
+                changes.invalidatedCachedSizes.append(index)
+            }
+            cachedSizes[index] = cell.bounds.height
+        }
+
+        var visibleIndices = [Int]()
+        var current: CGFloat = 0
+        for (i, size) in cachedSizes.enumerated() {
+            let cellTop = current
+            let cellBottom = current + size
+            defer {
+                current = cellBottom + 8
+            }
+
+            if cellTop > bottom {
+                break
+            }
+
+            if cellBottom >= top && cellTop <= bottom {
+                visibleIndices.append(i)
+            }
+        }
+
+        let sortedIndicesToInsert = visibleIndices.filter { visibleCells[$0] == nil }
+        changes.insertedIndices.append(contentsOf: sortedIndicesToInsert)
+
+        visibleCells = visibleCells.filter { (index, cell) in
+            guard !visibleIndices.contains(index) else { return true }
+            changes.removedIndices.append(index)
+            listView.removeArrangedSubview(cell)
+            cell.removeFromSuperview()
+            pool.insert(cell)
+            return false
+        }
+
+        for index in sortedIndicesToInsert {
+            let cell = nextCell()
+            cell.attach(id: index, size: sizes[index])
+            let indexInVisibleCells = visibleIndices.firstIndex(of: index)!
+            listView.insertArrangedSubview(cell, at: indexInVisibleCells)
+            visibleCells[index] = cell
+        }
+
+        func heightSum(for range: Range<Int>) -> CGFloat {
+            var sum: CGFloat = 0
+            for size in cachedSizes[range] {
+                sum += size + 8
+            }
+            return sum
+        }
+
+        topHeightConstraint.constant = heightSum(for: 0..<visibleIndices.first!)
+        bottomHeightConstraint.constant = heightSum(for: visibleIndices.last! + 1..<sizes.count)
+
+        if !changes.isEmtpy {
+            print("Following cells have sizes invalidated: \(changes.invalidatedCachedSizes)")
+            print("Following cells are removed and inserted, respectively: \(changes.removedIndices), \(changes.insertedIndices)")
+            print("Visible cells afterwards: \(visibleIndices).")
+        }
+
+        if let visualOffset = self.visualOffset {
+            let (index, cellOffset) = visualOffset
+            let before = heightSum(for: 0..<index)
+            scrollView.contentOffset.y = cellOffset + before
+        } else {
+            let index = visibleIndices[0]
+            let before = heightSum(for: 0..<index)
+            let cellOffset = scrollView.contentOffset.y - before
+            visualOffset = (index, cellOffset)
+        }
+    }
+
+    var timer: Timer!
+
+    override func viewDidLoad() {
+        configureHierarchy()
+        updateVisibleCells()
+
+        scrollView.delegate = self
+
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            print("About to update cell sizes.")
+            for cell in self.visibleCells.values {
+                cell.size.step()
+                cell.setNeedsUpdateConstraints()
+            }
+        }
+    }
+}
+
+extension RootViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        visualOffset = nil
+        let spaceLeft = scrollView.contentSize.height - scrollView.contentOffset.y - scrollView.bounds.height
+        print("Scrolled to \(scrollView.contentOffset.y), space left: \(spaceLeft)")
+        updateVisibleCells()
+    }
+
+    override func viewDidLayoutSubviews() {
+        updateVisibleCells()
+    }
+}
+
+extension UIResponder {
+    var rootViewController: RootViewController? {
+        if let casted = self as? RootViewController {
+            return casted
+        }
+        return next?.rootViewController
+    }
+}
+
+class HackedView: UIStackView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // A dirty way to notify the view controller.
+        rootViewController?.view.setNeedsLayout()
     }
 }
--- README.md
+++ README.md
@@ -19,3 +19,57 @@ Two commands are registered (Arrow Up and Down) to control a counter. They can o
 
 **Result**. It works very well. When key commands are available, it will take precedence over the default caret navigation behavior. When they are not, one can manipulate the caret as usual.
 
+## Auto Layout Based Virtualized List
+
+**Added**: 2020/05/23.
+
+**Description**. A virtualized list is a list that only maintains a list of cells visible to the user. This brings significant performance boost to Cocoa, and even modern browsers can benefit from it when DOM nodes get too many. The project aims to produce such a list with:
+
+- Auto Layout, so that we get self-sizing cells for free.
+- No use of `systemLayoutSizeFitting`, which, according to WWDC 2018 talk “High Performance Auto Layout”, creates a new layout engine, computes the layout, and discards the engine.
+
+**Setup**. For simplicity, I use a `UIStackView` to manage the list of cells on screen. Every one second, all visible cells get reassigned with a new random height. Two dummy views are added, one above and one below the stack view, with two height constraints attached to each. Whenever we need to layout subviews due to either a scroll event or cell size changes, contents inside the stack view are updated (unnecessary ones removed and missing ones inserted), and so are the two height constraints.
+
+**Result**. The code mostly works, but I am not very confident if my approach is correct or has optimal performance.
+
+When cell sizes change, I wait until stack view’s first layout pass is done before I update the list of visible cells, for otherwise we cannot get accurate latest cell bounds. To avoid unnecessary re-run the algorithm, I choose to override `layoutSubviews` for `UIStackView` and calls `setNeedsLayout` on the container view after `UIStackView` finishes its own work.
+
+This might cause the list update algorithm to execute for a few times, but it should stabilize very soon. Here is one example:
+
+```
+About to update cell sizes.
+Cell layout done for cell 8.
+Cell layout done for cell 7.
+Cell layout done for cell 6.
+Cell layout done for cell 5.
+Cell layout done for cell 4.
+Cell layout done for cell 3.
+Cell layout done for cell 2.
+Cell layout done for cell 1.
+Cell layout done for cell 0.
+Following cells have sizes invalidated: [1, 5, 7, 2, 4, 6, 3, 8, 0]
+Following cells are removed and inserted, respectively: [7, 6, 8], []
+Visible cells afterwards: [0, 1, 2, 3, 4, 5].
+About to update cell sizes.
+```
+
+Notice that since cells are removed, `UIStackView` will gets its `layoutSubviews` called again, but since there is no change to the list of visible cells, no log is produced, and we can confidently say that this thing stabilizes.
+
+Here is one worrisome example, my guess is that 6 has a dirty size, which triggers itself to be immediately updated. I am slightly confused about why `layoutSubviews` for cell 6 is executed after any cell list update. My not-so-educated guess would be that cell 6 has its bounds updated already and this method merely layout its subviews.
+
+```
+About to update cell sizes.
+Cell layout done for cell 5.
+Cell layout done for cell 4.
+Cell layout done for cell 3.
+Cell layout done for cell 2.
+Cell layout done for cell 0.
+Following cells have sizes invalidated: [5, 4, 2, 3, 0]
+Following cells are removed and inserted, respectively: [], [6]
+Visible cells afterwards: [0, 1, 2, 3, 4, 5, 6].
+Following cells have sizes invalidated: [6]
+Following cells are removed and inserted, respectively: [], []
+Visible cells afterwards: [0, 1, 2, 3, 4, 5, 6].
+Cell layout done for cell 6.
+```
+
